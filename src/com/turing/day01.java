50b1efe130500c2ae9c1ba743d29e9a648e5fc65
chenzhe <chenzhe@163.com>
2021-02-03 23:00:46 +0800

    添加你好

--- src/com/turing/day01.java
+++ src/com/turing/day01.java
@@ -4,5 +4,6 @@ public class day01 {
     public static void main(String[] args) {
         System.out.println("sayHi");
         System.out.println("hello");
+        System.out.println("你好");
     }
 }
