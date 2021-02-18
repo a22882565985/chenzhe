9e769cc58014849596dc5a10d1dd0f31375e3ff0
chenzhe <chenzhe@163.com>
2021-02-18 14:15:36 +0800

    添加长沙图灵

--- src/com/turing/day02.java
+++ src/com/turing/day02.java
@@ -4,6 +4,5 @@ public class day02 {
     public static void main(String[] args) {
         System.out.println("chenzhe");
         System.out.println("hi");
-        System.out.println("长沙图灵");
     }
 }
