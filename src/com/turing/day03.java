a3677da37719b712cc90902d8e95a1d6b59e8aad
chenzhe <chenzhe@163.com>
2021-02-18 15:11:08 +0800

    7777

--- src/com/turing/day03.java
+++ src/com/turing/day03.java
@@ -3,6 +3,6 @@ package com.turing;
 public class day03 {
     public static void main(String[] args) {
         System.out.println("turing");
-        System.out.println("6666");
+        System.out.println("7777");
     }
 }
