6e1f76830f72bc88a6b140bde8f48be446b6956a
chenzhe <chenzhe@163.com>
2021-02-18 14:51:50 +0800

    4444

--- src/com/turing/day02.java
+++ src/com/turing/day02.java
@@ -4,5 +4,6 @@ public class day02 {
     public static void main(String[] args) {
         System.out.println("chenzhe");
         System.out.println("chenzhe2");
+        System.out.println("4444");
     }
 }
