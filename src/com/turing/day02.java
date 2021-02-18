c6ce2e5e0b060043b130ba59842b2000ce3b8a11
chenzhe <chenzhe@163.com>
2021-02-18 14:31:05 +0800

    ccc

--- src/com/turing/day02.java
+++ src/com/turing/day02.java
@@ -4,6 +4,6 @@ public class day02 {
     public static void main(String[] args) {
         System.out.println("chenzhe");
         System.out.println("hi");
-        System.out.println("cz");
+        System.out.println("ccc");
     }
 }
