90ed9cbfe55ca55da575253ad1219fd55a3a3dcc
chenzhe <chenzhe@163.com>
2021-02-18 14:40:19 +0800

    chenzhe

--- src/com/turing/day02.java
+++ src/com/turing/day02.java
@@ -3,8 +3,5 @@ package com.turing;
 public class day02 {
     public static void main(String[] args) {
         System.out.println("chenzhe");
-        System.out.println("hi");
-        System.out.println("ccc");
-        System.out.println("cs");
     }
 }
